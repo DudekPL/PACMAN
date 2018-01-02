af0dcd2276b598ec69b2b78ed0414cfc617ae95f
Cezary <c.dudkiewicz1997@gmail.com>
2018-01-02 01:32:30 +0100

    nn

--- src/com/company/KeyActionMenu.java
+++ src/com/company/KeyActionMenu.java
@@ -3,6 +3,9 @@ package com.company;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class KeyActionMenu implements KeyListener {
     private final Game game;
@@ -24,7 +27,40 @@ public class KeyActionMenu implements KeyListener {
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         if (key == KeyEvent.VK_L) {
-            JOptionPane.showMessageDialog(null, "af", "Leaderboard", JOptionPane.PLAIN_MESSAGE);
+            StringBuilder message = new StringBuilder();
+            Scanner boardreader = null;
+            try{
+                File board = new File("lboard.txt");
+                if (board.createNewFile()) JOptionPane.showMessageDialog(null, "Plik został uszkodzony lub usunięty.\nStworzono nowy.", "Leaderboard", JOptionPane.PLAIN_MESSAGE);
+                boardreader = new Scanner(board);
+                if (boardreader.hasNextInt()){
+                    boardreader.nextLine();
+                }
+                int i = 1;
+                while (boardreader.hasNextLine()) {
+                    String name = boardreader.nextLine();
+                    String points = boardreader.nextLine();
+                    message.append(Integer.toString(i));
+                    message.append(". ");
+                    message.append(name);
+                    for (int j = 0; j < 21-name.length()-3-points.length(); j++) {
+                        message.append("_");
+                    }
+                    message.append(points);
+                    message.append("\n");
+                    i+=1;
+                }
+                JOptionPane.showMessageDialog(null, message.toString(), "Leaderboard", JOptionPane.PLAIN_MESSAGE);
+            }
+            catch (IOException exc){
+                JOptionPane.showMessageDialog(null, "Błąd odczytu", "Leaderboard", JOptionPane.ERROR_MESSAGE);
+            }
+            catch (SecurityException exc){
+                JOptionPane.showMessageDialog(null, "Plik jest chroniony", "Leaderboard", JOptionPane.ERROR_MESSAGE);
+            }
+            finally {
+                if (boardreader != null) boardreader.close();
+            }
         }
         if (key == KeyEvent.VK_ENTER) {
             frame.remove(menu);
